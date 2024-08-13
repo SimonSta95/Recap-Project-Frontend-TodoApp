703cdbe71b53ab2b214d6eb1781cb017f9eed0b6
Simon Staß <simonsta95@gmail.com>
2024-08-13 14:28:14 +0200

    Added possibility to add new Todos

--- frontend/src/components/AddTodo/AddTodo.tsx
+++ frontend/src/components/AddTodo/AddTodo.tsx
@@ -1,9 +1,25 @@
+import axios from "axios";
+import {useState} from "react";
+
+type Item = {
+     description: string,
+     status: string
+}
 
 export default function AddTodo() {
 
+    const [post, setPost] = useState<Item>({description: "", status:"OPEN"});
+
+    const addTodo = () => {
+        axios.post("api/todo",post)
+    }
+
     return(
         <>
-            <p>Add Todo</p>
+            <h3>Add a new Todo</h3>
+            <label>Description: </label>
+            <input type="text" onChange={(event) => {setPost({...post,description:event.target.value})}}/>
+            <button onClick={() => addTodo()}>Add</button>
         </>
     )
 }
